383fd4b6a999b5378de4b75bbb89845e59b42bfa
Jade Rigby <contact@jaderigby.com>
2019-10-29 16:22:34 -0600

    adding missing custom install file

---
+++ CustomInstallList.py
@@ -0,0 +1,20 @@
+def execute():
+    list = [
+          "Caskroom"
+        , "Git"
+        , "Node"
+        , "Atom"
+        , "iTerm"
+        , "Dropbox"
+        , "Slack"
+        , "Inkscape"
+        , "ImageMagick"
+        , "Chrome"
+        , "Firefox"
+        , "ImageAlpha"
+        , "MAMP"
+        , "ffmpeg"
+        , "FileZilla"
+        , "XQuartz"
+    ]
+    return list
